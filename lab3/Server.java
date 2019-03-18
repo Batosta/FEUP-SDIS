27bcaef6cb0399c53e7a4901dceccfdac528a0f3
João Nuno Ferreira <up201605330@g.uporto.pt>
2019-03-18 14:51:23 +0000

    Added the lab3

---
+++ lab3/Server.java
@@ -0,0 +1,43 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements Hello {
+
+    public Server() {}
+
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    public String lookup(String plate) {
+
+    }
+
+    public String register(String plate, String name){
+
+    }
+
+    public static void main(String args[]) {
+
+        if(args.length != 1){
+          System.out.println("Usage: Server <remote_object_name> ");
+          return;
+        }
+
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
