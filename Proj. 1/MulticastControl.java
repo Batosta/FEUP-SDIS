4743c90a8dbbd5b6387de2c0ba334ab9539802ee
João Nuno Ferreira <up201605330@g.uporto.pt>
2019-03-25 16:39:51 +0000

    Changed the control multicast

---
+++ Proj. 1/MulticastControl.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.net.InetAddress;
+
+// Control multicast + For delete protocol
+public class MulticastControl extends Multicast {
+
+
+	public MulticastControl(InetAddress address, int port){
+
+		super(address, port);
+		//missing specific information
+	}
+}
