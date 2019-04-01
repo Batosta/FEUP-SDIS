4340feb8284c21d096b2342c42a5131cf18ae0ce
batosta <up201605330@g.uporto.pt>
2019-04-01 16:34:08 +0100

    Continued with the Peer class

--- Proj. 1/Peer.java
+++ Proj. 1/Peer.java
@@ -2,31 +2,68 @@ import java.io.*;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
+import java.net.InetAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 public class Peer implements RMISystem{
 
-	//private static threads
+	private static int MAX_THREADS = 100;	// idk about this one
 
-	private static int ID;
+	private static int serverID;
+	private static double protocolVersion;
+	// private static String serviceAccessPoint;
 
-	private static MulticastControl MulticastControl;
-	private static MulticastBackup MulticastBackup;
-	private static MulticastRestore MulticastRestore;
+	// Private static threads:
+	private static MulticastControl MC;
+	private static MulticastBackup MDB;
+	private static MulticastRestore MDR;
+
+	private static ScheduledThreadPoolExecutor executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(MAX_THREADS);
 
 
-	private Peer(String addressMC, int portMC, String addressMDB, int portMDB, String addressMDR, int portMDR) {
+	private Peer(String ipAddressMC, int portMC, String ipAddressMDB, int portMDB, String ipAddressMDR, int portMDR) {
 
-		this.MulticastControl = new MulticastControl(addressMC, portMC);
-		this.MulticastBackup = new MulticastBackup(addressMDB, portMDB);
-		this.MulticastRestore = new MulticastRestore(addressMDR, portMDR);
+		try{
+
+			InetAddress mcIpAddress = InetAddress.getByName(ipAddressMC);
+			InetAddress mdbIpAddress = InetAddress.getByName(ipAddressMDB);
+			InetAddress mdrIpAddress = InetAddress.getByName(ipAddressMDR);
+
+			this.MC = new MulticastControl(mcIpAddress, portMC);
+			this.MDB = new MulticastBackup(mdbIpAddress, portMDB);
+			this.MDR = new MulticastRestore(mdrIpAddress, portMDR);
+		} catch (Exception exc) {
+
+ 			System.err.println(exc);
+		}
 	}
 
 	// TODO
 	public static void main(String[] args){
 
-	}
+		if(args.length != 9){
+			System.out.println("Usage:\n");
+			System.out.println("Peer <serverID> <protocolVersion> <serviceAccessPoint> <ipAddressMC> <portMC> <ipAddressMDB> <portMDB> <ipAddressMDR> <portMDR>");
+			return;
+		}
 
+		serverID = Integer.parseInt(args[0]);
+		protocolVersion = Double.parseDouble(args[1]);
+		String serviceAccessPoint = args[2];
+		String ipAddressMC = args[3];
+		int portMC = Integer.parseInt(args[4]);
+		String ipAddressMDB = args[5];
+		int portMDB = Integer.parseInt(args[6]);
+		String ipAddressMDR = args[7];
+		int portMDR = Integer.parseInt(args[8]);
+
+		Peer peer = new Peer(ipAddressMC, portMC, ipAddressMDB, portMDB, ipAddressMDR, portMDR);
+	}
+ 
 	// TODO
 	public void backupData(String path, int repDeg){
 
@@ -41,21 +78,24 @@ public class Peer implements RMISystem{
 
 	}
 
-	public static int getID(){
+	public int getServerID(){
 
-		return this.ID;
+		return this.serverID;
 	}
+	public double getProtocolVersion(){
 
-	public static MulticastControl getMulticastControl(){
-
-		return this.MulticastControl;
+		return this.protocolVersion;
 	}
-	public static MulticastBackup getMulticastBackup(){
+	public MulticastControl getMulticastControl(){
 
-		return this.MulticastBackup;
+		return this.MC;
 	}
-	public static MulticastRestore getMulticastRestore(){
+	public MulticastBackup getMulticastBackup(){
 
-		return this.MulticastRestore;
+		return this.MDB;
+	}
+	public MulticastRestore getMulticastRestore(){
+
+		return this.MDR;
 	}
 }
